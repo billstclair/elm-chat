1756241b8d0a782697c02ac12f1f9d1532009ff0
Bill St. Clair <billstclair@gmail.com>
2017-09-24 21:20:41 -0400

    Code done. Need to write example to test it.

--- ElmChat.elm
+++ ElmChat.elm
@@ -10,7 +10,7 @@
 ----------------------------------------------------------------------
 
 
-module ElmChat exposing ( Settings, makeSettings, chat )
+module ElmChat exposing ( Settings, makeSettings, chat, addChat, inputBox )
 
 {-| This module contains a simple chat component
 that you can easily add to your Elm user interface.
@@ -19,7 +19,7 @@ that you can easily add to your Elm user interface.
 @docs Settings
 
 # Functions
-@docs makeSettings, chat
+@docs makeSettings, chat, addChat, inputBox
 -}
 
 import Html exposing ( Html, Attribute
@@ -33,15 +33,16 @@ import Html.Attributes exposing ( value, size, maxlength, href, src, title
                                 , width, height, class
                                 , readonly, id
                                 )
-import Html.Events exposing ( onClick, onInput, onFocus, onCheck, on, keyCode )
+import Html.Events exposing ( onClick, onInput, on, keyCode )
 import Dom.Scroll as Scroll
 import Task
+import Json.Decode as Json
 
 {-| Settings for the chat component
 -}
 type alias Settings msg =
     { fontSize : Int
-    , chat : String
+    , text : String
     , input : String
     , scroll : Float
     , id : String
@@ -54,12 +55,6 @@ type alias Updater msg =
 type TheUpdater msg
     = TheUpdater (Updater msg)
 
-{-| Create a chat component.
--}
-chat : Settings msg -> List (Attribute msg) -> Html msg
-chat settings attributes =
-    text ""
-
 defaultFontSize : Int
 defaultFontSize =
     20
@@ -71,7 +66,7 @@ updater will be called to generate messages to update the Settings in your Model
 makeSettings : String -> Updater msg -> Settings msg
 makeSettings id updater =
     { fontSize = defaultFontSize
-    , chat = ""
+    , text = ""
     , input = ""
     , scroll = -1000
     , id = id
@@ -89,8 +84,8 @@ noUpdate settings =
     update settings Cmd.none
 
 scroll : Settings msg -> Float -> msg
-scroll settings scroll =
-    let s = scroll-1
+scroll settings amount =
+    let s = amount-1
     in
         if s >= settings.scroll then
             update
@@ -116,14 +111,99 @@ setFontSize settings dir =
             { settings | fontSize = newsize }
             Cmd.none
 
+br : Html msg
+br =
+    Html.br [][]
+
+chatSizeButton : Settings msg -> (Int, String, String) -> List (Html msg)
+chatSizeButton settings (size, title_, label) =
+    [ button [ onClick <| setFontSize settings size
+             , title title_
+             --, class "chatsizebutton"
+             ]
+          [ text label ]
+    , br
+    ]
+
+{-| Create a chat component.
+-}
+chat : Settings msg -> Html msg
+chat settings =
+    table []
+        [ tr []
+              [ td [ class "chatsizecolumn" ]
+                    <| List.concatMap (chatSizeButton settings)
+                    [ (1, "Increase chat size", "^")
+                    , (0, "Default chat size", "O")
+                    , (-1, "Decrease chat size", "v")
+                    ]
+              , td []
+                  [ textarea [ id "chat"
+                             , class "chat"
+                             , readonly True
+                             ]
+                        -- TODO: make player names bold.
+                        [ text <| settings.text ]
+                  ]
+              ]
+        ]
+
+{-| Add a line to the chat box.
+-}
+addChat : Settings msg -> String -> msg
+addChat settings message =
+    let newText = if settings.text == "" then
+                      settings.text
+                  else
+                      settings.text ++ "\n"
+        newSettings = { settings | text = newText ++ message }
+
+    in
+        update
+            newSettings
+            <| Task.attempt (\res ->
+                                 case res of
+                                     Ok amount -> scroll settings amount
+                                     Err _ -> noUpdate settings
+                            )
+                <| Scroll.y "chat"
+
 ---
 --- Input boxes
 ---
 
-keyDown : (String -> Settings msg -> msg) -> Settings msg -> Int -> msg
+type alias Sender msg =
+    String -> Settings msg -> msg
+
+keyDown : Sender msg -> Settings msg -> Int -> msg
 keyDown sender settings keycode =
     if keycode == 13 then
         sender settings.input { settings | input = "" }
     else
         noUpdate settings
 
+send : Sender msg -> Settings msg -> msg
+send sender settings =
+    sender settings.input { settings | input = "" }
+
+onKeydown : (Int -> msg) -> Attribute msg
+onKeydown tagger =
+  on "keydown" (Json.map tagger keyCode)
+
+{-| Create a text input area of the given textSize (characters),
+with a send button with the given buttonText.
+-}
+inputBox : Int -> String -> Sender msg -> Settings msg -> Html msg
+inputBox textSize buttonText sender settings =
+    span []
+        [ input [ type_ "text"
+                , onInput (\text -> noUpdate { settings | text = text } )
+                , onKeydown <| keyDown sender settings
+                , size textSize
+                , value settings.input
+                ]
+              []
+        , text " "
+        , button [ onClick <| send sender settings ]
+            [ text buttonText ]
+    ]
